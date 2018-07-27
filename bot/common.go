4d090bcdbf8b6dddee45a9794417be15171b3e88
Luqman Arifin <l.arifin.siswanto@gmail.com>
2018-07-27 11:11:41 +0700

    fix indent

--- bot/common.go
+++ bot/common.go
@@ -23,7 +23,7 @@ func generateUpcomingContestsMessage(clistService *clist.Service, startFrom, sta
 	for _, contest := range contests {
 		str := ""
 		if buffer.Len() > 0 {
-			str = "\n" 
+			str = "\n"
 		}
 		str = str + fmt.Sprintf("- %s. Starts at %s. Link: %s", contest.Name, contest.StartDate.In(tz).Format("Jan 2 15:04 MST"), contest.Link)
 		if buffer.Len()+len(str) > limit {
